df2648fadcacc0331c9a2109926eef2dc740c9f1
SnowflakeIsABumbleBee <97166092+SnowflakeIsABumbleBee@users.noreply.github.com>
2025-05-28 18:00:50 +0200

    name hinzugefügt

--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,8 @@
-public class App {
+
+public class Robot {
+
+    private String name;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
