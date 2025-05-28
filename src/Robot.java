0517aba448c6ce5e5acb64fbfda2a4da2fbf2dd2
SnowflakeIsABumbleBee <97166092+SnowflakeIsABumbleBee@users.noreply.github.com>
2025-05-28 18:41:32 +0200

    fail

--- src/Robot.java
+++ src/Robot.java
@@ -1,9 +1,25 @@
 
+import java.util.Random;
+
 public class Robot {
 
-    private String name;
+    private final String name;
+    private int battery;
+    private int distance;
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Robot alpha = new Robot("Alpha");
+        Robot betha = new Robot("Betha");
+
     }
+
+    public Robot(String string) {
+        this.name = name;
+    }
+
+    public void runStep() {
+        Random rand = new Random();
+
+    }
+
 }
